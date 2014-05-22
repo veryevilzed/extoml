f19b19f25ad1871e3556aabaa1ea429ced782265
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2014-05-22 08:59:51 +0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Extoml
+
+** TODO: Add description **
---
+++ lib/extoml.ex
@@ -0,0 +1,15 @@
+defmodule ExToml do
+  
+  defp to_map(list) when is_list(list) do
+    list |> Enum.reduce %{}, fn({key, value}, acc) -> Dict.put acc, key, to_map(value) end
+  end
+
+  defp to_map(list), do: list
+
+  def parse(text) do
+    case :etoml.parse(text) do
+      {:ok, toml} -> {:ok, to_map(toml) }
+      err -> err
+    end
+  end
+end
